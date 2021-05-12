4ae292af41756679d58df1225714cdcdbf7c0c32
SamNzz <samuel.nzonzi@hotmail.com>
2021-05-12 13:54:05 +0200

    environnement

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-#Ajout et test d'une API avec node.js
+# API Restfull d'une librairie (pour le fun)
+
+## Les exercices
+
+### Exo 1 - Mettre en place un README.md
+
+1. Cloner ce projet dans le répertoire de votre choix
+2. Ouvrir le projet avec vscode
+3. Créer un fichier README.md (avec vscode) et placez une courte
+   description du projet
+4. Dans un terminal (vscode) faire un commit
+5. Faire un « push » sur github
+6. Envoyer le lien du projet github sur le chat
+
+### Exo 2 - Mettre en place un package.json
+
+1. Lancer la commande `npm init`
+2. Renseigner dans "entry point" le chemin `src/index.js`
+3. Ajouter la ligne `"type": "module",` après la clefs `main` dans le `package.json`
+4. Faire un commit et pousser sur github
+
+### Exo 3 - Lancer un script js avec nodejs
+
+1. Créer le fichier `src/index.js` avec un console.log
+2. Ajouter un script dans `package.json` avec
+   la configuration suivante:
+
+```js
+"scripts": {
+    "start": "node src/index.js"
+}
+```
+
+3. Vous pouvez tester en lancant la commande
+   `yarn start` ou bien `npm start`
+4. Faire un commit et "push" sur github
+
+### Exo 4 - Installer Fastify
+
+1. On lance l'installation de fastify avec la commande `yarn add fastify` (vous pouvez
+   vérifier l'installation en regardant votre `package.json`)
+2. On ignore le versionning du répertoire `node_modules`:
+   1. créé un fichier `.gitignore` à la racine du projet
+   2. Ajouter la ligne `node_modules` dans le fichier `.gitignore`
+3. On commit et push sur github
+
+### Exo 5 - Un server fastify
+
+1. Dans `src/index.js` ajouter le code permettant de démarer un server sur le port
+   3000
+2. Vous pouvez lancer la commander `yarn start` pour tester votre server (vous pouvez
+   appuyer sur Ctrl-C afin de quitter le server)
+3. On commit et on push
+
+### Exo 6 - afficher un text de bienvenue sur la route "`/`"
+
+1. Dans `src/index.js` ajouter une route get avec le chemin `/` et retourner
+   un objet json de votre choix
+2. Vous pouvez tester en demarant votre server (si dèja lancé vous pouvez l'arréter avec
+   la touche Ctrl-C dans votre terminal, pour lancer le server faire `yarn start`).
+3. On commit et on push
+
+### Exo 7 - Installer fastify mongodb extension
+
+1. Installer la paquet: `fastify-mongodb` avec la commande `yarn add fastify-mongodb`
+2. Configurer fastify mongo dans le fichier `index.js` (Vous pouvez utiliser l'url: `mongodb+srv://MyTodoApp:MyTodoApp@cluster0.obacx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+3. Vous pouvez tester en lancant votre server (faire Ctrl-C si le server est dèja lancé, pour lancer le server la commande est `yarn start`)
+4. On commit et on push sur github
+
+### Exo 8 - Retourner tout les livres d'une BDD
+
+1. Créer la route `/books`
+2. On récupére la collection "books" depuis mongodb
+3. On récupére tout les livres depuis notre collection
+4. On retourne les livres
+5. On commit et on push
+
+### Exo 9 - Installer et mettre en place nodemon
+
+1. Installer nodemon avec la commande `yarn add nodemon`
+2. Modifier le script `start` dans le `package.json` pour utiliser nodemon
+3. On commit et on push
--- src/index.js
+++ src/index.js
@@ -1,1 +1,175 @@
-console.log('is Mario');
+// On importe la libraire fastify
+import fastify from 'fastify'
+import fastifyMongo from 'fastify-mongodb'
+
+// On créé une application fastify en utilisant
+// L'import de notre librairie. On configure
+// fastify pour afficher des logs
+const app = fastify({ logger: true })
+
+// On connécte la base de données MongoDB
+app.register(fastifyMongo, {
+    url: 'mongodb+srv://MyTodoApp:MyTodoApp@cluster0.obacx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+})
+
+// On créé une route fastify sur l'URI "/"
+app.get('/', async() => {
+    return { text: "Welcome !" }
+})
+
+// On créé une route qui retourne tout les livres de notre
+// base de données (MongoDB)
+app.get('/books', async() => {
+    // Mongo est une base de données qui contient des collection
+    // (c'est un peu comme des tables en SQL)
+    // Ici on récupére la collection "books"
+    const collection = app.mongo.db.collection('books')
+
+    // Sur cette collection nous pouvons utiliser plusieurs
+    // fonctions. Ici, nous allons récupérer TOUT les livres
+    const books = await collection.find().toArray()
+
+    // Nous retournons tout les livres de la base de données
+    return books
+})
+
+
+// On créé une route qui retourne qu'une livre par son
+// identifiant
+app.get('/books/:id', async(request, reply) => {
+    // On récupére l'identifiant rentré dans notre url
+    const id = request.params.id
+
+    // On récupére notre collection mongodb
+    const collection = app.mongo.db.collection('books')
+
+    // On a'ssure qu'il n'y ai pas d'erreur
+    try {
+        // Nous récupérons le livre avec l'id spécifié dans la route
+        const book = await collection.findOne({ _id: new app.mongo.ObjectId(id) })
+
+        // Si il n'y a pas de livre
+        if (!book) {
+            throw new Error('This books does not exists')
+        }
+
+        // Si tout est ok on retourne le livre
+        return book
+    } catch (error) {
+        // Ici, si la moindre erreur est survenu à l'intérieur
+        // du block try nous exécutons le code suivant:
+
+        // On change le status code par 404 (Not Found)
+        reply.status(404)
+
+        // On retourne le message de l'erreur
+        return { error: error.message }
+    }
+})
+
+// Mise à jour d'un livre
+app.patch('/books/:id', async(request) => {
+    const id = request.params.id
+    const updateFields = request.body
+        // Pour mettre à jour un livre avec MongoDB
+        // il faut utiliser : await collection.updateOne({ _id: new app.mongo.ObjectId(id) }, nouveauLivre)
+    const collection = app.mongo.db.collection('books')
+
+    // Nous mettons le livre avec l'identifiant donnée
+    // en luis spécifiant les champs à changer en
+    // second argument
+    await collection.updateOne(
+        // Ici on spécifie des "Query" qui nous permettent
+        // de définir le livre à mettre à jour
+        { _id: new app.mongo.ObjectId(id) },
+        // On specifie dans le clefs "$set" les champs à mettre
+        // à jour
+        { $set: updateFields },
+    )
+
+    // On récupére le livre mis à jour dans la base de données
+    const book = await collection.findOne({
+        _id: new app.mongo.ObjectId(id),
+    })
+
+    // On retourne le livre
+    return book
+})
+
+// Suppression d'un livre
+app.delete('/books/:id', async(request) => {
+    const id = request.params.id
+        // Pour supprimer un livre avec MongoDB
+        // il faut utiliser : await collection.deleteOne({ _id: new app.mongo.ObjectId(id) })
+    const collection = app.mongo.db.collection('books')
+
+    await collection.deleteOne({
+        _id: new app.mongo.ObjectId(id)
+    })
+
+    return null
+})
+
+// On déclare un schèma qui nous permettra de valider
+// les données envoyé dans la request POST /books
+const createBookSchema = {
+    type: 'object',
+    properties: {
+        title: { type: 'string' },
+        description: { type: 'string' },
+        image: { type: 'string' },
+        author: { type: 'string' },
+        price: { type: 'number' },
+        stars: { type: 'number' },
+    },
+    required: [
+        'title',
+        'description',
+        'image',
+        'author',
+        'price',
+        'stars'
+    ]
+}
+
+// On créé un route qui nous permettra d'ajouter (de créer) un
+// nouveau livre et nous lui attachons un schema !
+app.post('/books', {
+    schema: {
+        body: createBookSchema
+    }
+}, async(request) => {
+    // Nous récupérons toutes les données qu'il y a dans le corp
+    // d'une requête. Cela correspond à notre livre
+    const book = request.body
+
+    // Pour enregistrer le livre dans ma BDD (mongodb)
+    // j'ai besoin de la collection
+    const collection = app.mongo.db.collection('books')
+
+    // On enregistre le livre dans la base de données
+    const result = await collection.insertOne(book)
+
+    // A l'intérieur de result on as tout les opérations qui ont
+    // étaient enregistré.
+    // Pour accéder à tout ce qui a été enregistré dans la BDD
+    // on utilise result.ops
+    // Ici on as inséré un seul élément, result.ops sera donc
+    // un tableaux avec une seul valeur à l'intérieur: Notre livre.
+
+    // On retourne le livre qui a été enregistré dans la base de
+    // données
+    return result.ops[0]
+})
+
+// Cette fonction démarre notre server d'api
+const start = async() => {
+    console.log('Lancement de notre server ...')
+
+    await app.listen(3000)
+
+    console.log('Le server est lancé, vous pouvez visiter: http://localhost:3000')
+}
+
+// Lancement de la fonction de démarage
+start()
